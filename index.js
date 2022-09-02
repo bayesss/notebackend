229441580807d4e2b380bb21c37a5dc52d450e14
Bayes <sshuai.yang@gmail.com>
2022-09-02 22:08:41 +0800

    调整use中间件顺序

--- index.js
+++ index.js
@@ -1,131 +1,132 @@
-const { response } = require('express')
-const express = require('express')
-const app = express()
-require('dotenv').config()
+const { response } = require("express");
+const express = require("express");
+const app = express();
+require("dotenv").config();
 
-app.use(express.static('build'))
-app.use(express.json())
+app.use(express.static("build"));
+app.use(express.json());
 const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
-}
-app.use(requestLogger)
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+app.use(requestLogger);
 
-const cors = require('cors')
-app.use(cors())
+const cors = require("cors");
+app.use(cors());
 
-const Note = require('./models/note')
+const Note = require("./models/note");
 
+app.get("/", (request, response) => {
+  response.send("<h1>Hello world!</h1>");
+});
+app.get("/about", (request, response) => {
+  response.send("<h1>Hello about!</h1>");
+});
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello world!</h1>')
-})
-app.get('/about', (request, response) => {
-    response.send('<h1>Hello about!</h1>')
-})
+app.get("/api/notes", (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
+});
 
-app.get('/api/notes', (request, response) => {
-    Note.find({}).then(notes => {
-        response.json(notes)
+app.get("/api/notes/:id", (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
     })
-})
+    .catch((error) => {
+      next(error);
+    });
+});
 
-app.get('/api/notes/:id', (request, response, next) => {
-    Note.findById(request.params.id)
-        .then(note => {
-            if (note) {
-                response.json(note)
-
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => { next(error) })
-})
-
-app.delete('/api/notes/:id', (request, response, next) => {
-    Note.findByIdAndRemove(request.params.id)
-        .then(resulft => {
-            response.status(204).end()
-        })
-        .catch(error => { next(error) })
-})
-
-
-app.post('/api/notes', (request, response, next) => {
-    const body = request.body
-
-    // if (body.content === undefined) {
-    //     return response.status(400).json({
-    //         error: 'content missing'
-    //     })
-    // }
-
-    const note = new Note({
-        content: body.content,
-        important: body.important || false,
-        date: new Date(),
+app.delete("/api/notes/:id", (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then((resulft) => {
+      response.status(204).end();
     })
+    .catch((error) => {
+      next(error);
+    });
+});
 
-    note.save().then(savedNote => {
-        response.json(savedNote)
+app.post("/api/notes", (request, response, next) => {
+  const body = request.body;
+
+  // if (body.content === undefined) {
+  //     return response.status(400).json({
+  //         error: 'content missing'
+  //     })
+  // }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  });
+
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
     })
-        .catch(error => next(error))
+    .catch((error) => next(error));
+});
 
+app.put("/api/notes/:id", (request, response, next) => {
+  const body = request.body;
 
-})
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
 
-app.put('/api/notes/:id', (request, response, next) => {
-    const body = request.body
-
-    const note = {
-        content: body.content,
-        important: body.important,
-    }
-
-    Note.findByIdAndUpdate(request.params.id, note, { new: true, runValidators: true, context: 'query' })
-        .then(updatedNote => {
-            response.json(updatedNote)
-        })
-        .catch(error => next(error))
-})
-
-
+  Note.findByIdAndUpdate(request.params.id, note, {
+    new: true,
+    runValidators: true,
+    context: "query",
+  })
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
+});
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-}
-app.use(unknownEndpoint)
+  response.status(404).send({ error: "unknown endpoint" });
+};
+app.use(unknownEndpoint);
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+  console.error(error.message);
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
 
-
-    next(error)
-}
+  next(error);
+};
 
 // this has to be the last loaded middleware.
-app.use(errorHandler)
+app.use(errorHandler);
 
 // const app = http.createServer((request, response) => {
 //     response.writeHead(200, { 'Content-Type': 'application/json' })
 //     response.end(JSON.stringify(notes))
 // })
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`);
-})
-
+  console.log(`server is running on port ${PORT}`);
+});
 
 // app.listen(PORT)
 // console.log(`Server running on port ${PORT}`);
